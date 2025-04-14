2acf9e10edd8c840b33f2206596414de5da187cc
adaytynys <aday.tynys@ffins.lz>
2025-04-14 20:55:35 +0500

    some fix of git logs

---
+++ router/authRouter.js
@@ -0,0 +1,23 @@
+const AuthRouter = require("express");
+const router = new AuthRouter();
+const authController = require("../controller/authController");
+const { check } = require("express-validator");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+
+router.post(
+    "/registration",
+    [
+        check("username", "Имя пользователя не может быть пустым").notEmpty(),
+        check(
+            "password",
+            "Пароль должен быть больше 4 и меньше 10 символов"
+        ).isLength({ min: 4, max: 10 }),
+    ],
+    authController.registration
+);
+router.post("/login", authController.login);
+router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
+router.get("/user", authMiddleware, authController.getUser);
+
+module.exports = router;
