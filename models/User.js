ebee401168b880617334ea7faeb88601004495f0
aday.tynys <aday.tynys@ffins.kz>
2025-06-27 19:20:48 +0500

    feat add comments

--- models/User.js
+++ models/User.js
@@ -6,6 +6,18 @@ const User = new Schema({
     role: { type: String, ref: "Role" },
     avatar: { type: String, required: false },
 
+    profile: {
+        firstName: { type: String },
+        lastName: { type: String },
+        birthDate: { type: Date },
+        about: { type: String },
+        telegram: { type: String },
+        instagram: { type: String },
+        twitter: { type: String }
+    },
+
+
+
     userBg: { type: String, required: false },
     likedBooks: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
     library: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
